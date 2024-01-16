64f23b839e43ad420b441efd1d2a1592f1fa814b
maged <maged.mohamed@lavaloon.com>
2024-01-16 14:42:36 +0200

    updated for business settings

--- ksa_compliance/ksa_compliance/doctype/zatca_business_settings/zatca_business_settings.js
+++ ksa_compliance/ksa_compliance/doctype/zatca_business_settings/zatca_business_settings.js
@@ -1,8 +1,29 @@
-// Copyright (c) 2024, Lavaloon and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("ZATCA Business Settings", {
+  setup: function (frm) {
+    filter_company_address(frm);
+  },
+  company: function (frm) {
+    filter_company_address(frm);
+  },
+});
 
-// frappe.ui.form.on("ZATCA Business Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function filter_company_address(frm) {
+  frappe.call({
+    method:
+      "ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_business_settings.fetch_company_addresses",
+    args: {
+      company_name: frm.doc.company,
+    },
+    callback: function (r) {
+      if (r.message) {
+        frm.set_query("company_address", function () {
+          return {
+            filters: {
+              name: ["in", r.message],
+            },
+          };
+        });
+      }
+    },
+  });
+}
--- ksa_compliance/ksa_compliance/doctype/zatca_business_settings/zatca_business_settings.py
+++ ksa_compliance/ksa_compliance/doctype/zatca_business_settings/zatca_business_settings.py
@@ -2,8 +2,21 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ZATCABusinessSettings(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def fetch_company_addresses(company_name):
+    # company_list = frappe.db.sql(
+    #     ''' SELECT parent
+    #     	FROM `tabDynamic Link`
+    #         WHERE link_name = '{}' '''.format(company_name))
+    company_list_dict = frappe.get_all("Dynamic Link", filters={"link_name": company_name}, fields=["parent"])
+    company_list = [address.parent for address in company_list_dict]
+    print(company_list)
+    return company_list
