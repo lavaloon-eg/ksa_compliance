ad1b5829f1d0229e93d3949f8251d44a7eb30d87
Salama <mohamed.salama@lavaloon.com>
2024-01-24 15:16:21 +0200

    add simplified tax invoice xml template

--- ksa_compliance/generate_xml.py
+++ ksa_compliance/generate_xml.py
@@ -2,20 +2,23 @@ import frappe
 import json
 
 
-# import lxml.etree as ET
-
-
 @frappe.whitelist(allow_guest=True)
-def generate_xml():
-    data = frappe.request.data or {}
-    data = json.loads(data)
+def generate_xml(input_data: dict = None):
+    data = input_data or frappe.request.data
+    if not isinstance(data, dict):
+        data = json.loads(data)
+
+    invoice_type = data.get("invoice_type")
+    template = "simplified_e_invoice.xml" if invoice_type.lower() == "simplified" else "standard_e_invoice.xml"
 
     # render XML Template
     invoice_xml = frappe.render_template(
-        "ksa_compliance/ksa_compliance/e_invoice.xml",
-        context={"invoice": data, "seller": {}, "buyer": {}, "business_settings": {}},
+        f"ksa_compliance/templates/{template}",
+        context={"invoice": data.get("invoice"), "seller": data.get("seller"), "buyer": data.get("buyer"),
+                 "business_settings": data.get("business_settings")},
         is_path=True
     )
     invoice_xml = invoice_xml.replace("&", "&amp;")
+    invoice_xml = invoice_xml.replace("\n", "")
 
     return invoice_xml
