eec40514d615b01c5d9f02867fe0f3407eba13a8
Salama <mohamed.salama@lavaloon.com>
2024-01-25 20:01:58 +0200

    reformat code and add extra fields

--- ksa_compliance/ksa_compliance/doctype/sales_invoice_additional_fields/sales_invoice_additional_fields.py
+++ ksa_compliance/ksa_compliance/doctype/sales_invoice_additional_fields/sales_invoice_additional_fields.py
@@ -8,8 +8,11 @@ from ksa_compliance.output_models.business_settings_output_model import Einvoice
 
 class SalesInvoiceAdditionalFields(Document):
     def on_submit(self):
-        e_invoice = Einvoice(sales_invoice_additional_fields_doc=self, invoice_type="Simplified")
-        print(e_invoice.result, e_invoice.error_dic)
+        # Construct the EInvoice output data
+        e_invoice = construct_einvoice_data(self)
         frappe.log_error("ZATCA Result LOG", message=e_invoice.result)
         frappe.log_error("ZATCA Error LOG", message=e_invoice.error_dic)
-        sad
+
+
+def construct_einvoice_data(additional_fields_doc):
+    return Einvoice(sales_invoice_additional_fields_doc=additional_fields_doc, invoice_type="Simplified")
--- ksa_compliance/standard_doctypes/sales_invoice.py
+++ ksa_compliance/standard_doctypes/sales_invoice.py
@@ -2,25 +2,19 @@ import frappe
 
 
 def create_sales_invoice_additional_fields_doctype(self, method):
-    print(self, method)
-    print(type(self))
-    print(self.as_dict())
-    customer_address_doc = frappe.get_doc("Address", self.customer_address)
-    print(customer_address_doc.as_dict())
-    print("\n\n\n" + "ss" + "\n\n\n")
-
-    sales_invoice_additional_fields_dict = {
-        "doctype": "Sales Invoice Additional Fields",
-        "sales_invoice": self.name,
-        "buyer_street_name": customer_address_doc.get("address_line1"),
-        "buyer_additional_street_name": customer_address_doc.get("address_line2"),
-        "buyer_building_number": customer_address_doc.get("custom_building_number"),
-        "buyer_additional_number": "not available for now",
-        "buyer_city": customer_address_doc.get("city"),
-        "buyer_postal_code": customer_address_doc.get("pincode"),
-        "buyer_district": customer_address_doc.get("custom_area"),
-        "buyer_country_code": customer_address_doc.get("country"),
-    }
-    additional_fields_doc = frappe.get_doc(sales_invoice_additional_fields_dict)
-    additional_fields_doc.save()
-    return
+    si_additional_fields_doc = frappe.new_doc("Sales Invoice Additional Fields")
+    si_additional_fields_doc.sales_invoice = self.name
+    if self.customer_address:
+        customer_address_doc = frappe.get_doc("Address", self.customer_address)
+        address_info = {
+            "buyer_additional_number": "not available for now",
+            "buyer_street_name": customer_address_doc.get("address_line1"),
+            "buyer_additional_street_name": customer_address_doc.get("address_line2"),
+            "buyer_building_number": customer_address_doc.get("custom_building_number"),
+            "buyer_city": customer_address_doc.get("city"),
+            "buyer_postal_code": customer_address_doc.get("pincode"),
+            "buyer_district": customer_address_doc.get("custom_area"),
+            "buyer_country_code": customer_address_doc.get("country"),
+        }
+        si_additional_fields_doc.update(address_info)
+    si_additional_fields_doc.insert()
