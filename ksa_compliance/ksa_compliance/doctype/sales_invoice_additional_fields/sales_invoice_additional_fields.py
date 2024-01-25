c6045aec132e2decd10957bfd25ea31e6f36530e
Salama <mohamed.salama@lavaloon.com>
2024-01-25 20:17:58 +0200

    Save generated XML into a file

--- ksa_compliance/ksa_compliance/doctype/sales_invoice_additional_fields/sales_invoice_additional_fields.py
+++ ksa_compliance/ksa_compliance/doctype/sales_invoice_additional_fields/sales_invoice_additional_fields.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from ksa_compliance.output_models.business_settings_output_model import Einvoice
+from ksa_compliance.generate_xml import generate_xml_file
 
 
 class SalesInvoiceAdditionalFields(Document):
@@ -12,6 +13,7 @@ class SalesInvoiceAdditionalFields(Document):
         e_invoice = construct_einvoice_data(self)
         frappe.log_error("ZATCA Result LOG", message=e_invoice.result)
         frappe.log_error("ZATCA Error LOG", message=e_invoice.error_dic)
+        generate_xml_file(e_invoice.result)
 
 
 def construct_einvoice_data(additional_fields_doc):
