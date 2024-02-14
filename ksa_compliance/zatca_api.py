3a9066cf536459d02ec82dbbb92b553b7acb04c3
Muhammad Haggag <muhammad.haggag@lavaloon.com>
2024-02-14 20:08:04 +0200

    Improve ZATCA API error handling

--- ksa_compliance/zatca_api.py
+++ ksa_compliance/zatca_api.py
@@ -4,7 +4,7 @@ from urllib.parse import urljoin
 
 import requests
 from frappe.utils.logger import get_logger
-from requests import HTTPError
+from requests import HTTPError, Response, JSONDecodeError
 from requests.auth import HTTPBasicAuth
 from result import Result, Ok, Err
 
@@ -73,11 +73,27 @@ def api_call(server: str, path: str, headers: Dict[str, str], body: Dict[str, st
         if e.response.text:
             logger.info(f'Response: {e.response.text}')
 
-        if e.response.status_code == 400:
-            data = e.response.json()
-            errors = cast(List[str], data['errors'])
-            return Err(', '.join(errors))
-        return Err(str(e))
+        return Err(try_get_response_error(e.response, str(e)))
     except Exception as e:
         logger.error('An unexpected exception occurred', exc_info=e)
         return Err(str(e))
+
+
+def try_get_response_error(response: Response, default_error: str) -> str:
+    """Tries to extract an error from a ZATCA response. The sandbox API isn't consistent in how it reports errors,
+    so this method tries a number of approaches based on the observed error responses."""
+    try:
+        data = response.json()
+        if response.status_code == 400:
+            errors = cast(List[str], data.get('errors', []))
+            if not errors:
+                errors.append(default_error)
+            return ', '.join(errors)
+
+        if response.status_code == 500:
+            return data.get('message', default_error)
+
+        return default_error
+    except JSONDecodeError:
+        # If the response is not JSON, we return the content itself as the error
+        return response.text
