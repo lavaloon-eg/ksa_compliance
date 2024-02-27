5bf88b14d53365d3ff18d6a64a3f3215d2c89402
Salama <mohamed.salama@lavaloon.com>
2024-02-27 21:37:00 +0200

    fix batch

--- ksa_compliance/patches/_2024_02_27_add_counting_docs_for_existing_settings.py
+++ ksa_compliance/patches/_2024_02_27_add_counting_docs_for_existing_settings.py
@@ -2,24 +2,33 @@ import frappe
 
 
 def execute():
-    first_previous_hash = "NWZlY2ViNjZmZmM4NmYzOGQ5NTI3ODZjNmQ2OTZjNzljMmRiYzIzOWRkNGU5MWI0NjcyOWQ3M2EyN2ZiNTdlOQ=="
     existing_settings = frappe.db.get_all("ZATCA Business Settings", ["name", "company"])
     if existing_settings:
         for setting in existing_settings:
+            max_invoice_counter = 0
+            invoice_hash = "NWZlY2ViNjZmZmM4NmYzOGQ5NTI3ODZjNmQ2OTZjNzljMmRiYzIzOWRkNGU5MWI0NjcyOWQ3M2EyN2ZiNTdlOQ=="
 
-            additional_doc_data = frappe.db.sql("""
-                SELECT MAX(ad.invoice_counter) AS max_invoice_counter, ad.invoice_hash 
+            max_invoice_counter = frappe.db.sql("""
+                SELECT MAX(ad.invoice_counter) AS max_invoice_counter
                 FROM `tabSales Invoice Additional Fields` AS ad
                 LEFT JOIN `tabSales Invoice` AS si
                 ON ad.sales_invoice = si.name
-            """, as_dict=1)
+                WHERE si.company = %(company)s
+            """, {"company": setting.company}, as_dict=1)[0].max_invoice_counter or 0
+
+            if max_invoice_counter:
+                invoice_hash = frappe.db.sql("""
+                    SELECT ad.invoice_hash
+                    FROM `tabSales Invoice Additional Fields` AS ad
+                    LEFT JOIN `tabSales Invoice` AS si
+                    ON ad.sales_invoice = si.name
+                    WHERE si.company = %(company)s
+                    AND ad.invoice_counter = %(max_counter)s            
+                """, {"max_counter": max_invoice_counter, "company": setting.company}, as_dict=1)[0].invoice_hash
 
             if not frappe.db.exists("ZATCA Invoice Counting Settings", setting.name):
                 invoice_counting_doc = frappe.new_doc("ZATCA Invoice Counting Settings")
                 invoice_counting_doc.business_settings_reference = setting.name
-                invoice_counting_doc.invoice_counter = (
-                    additional_doc_data[0].max_invoice_counter if additional_doc_data[0].max_invoice_counter else 0)
-                invoice_counting_doc.previous_invoice_hash = (
-                    additional_doc_data[0].invoice_hash if additional_doc_data[
-                        0].invoice_hash else first_previous_hash)
+                invoice_counting_doc.invoice_counter = max_invoice_counter
+                invoice_counting_doc.previous_invoice_hash = invoice_hash
                 invoice_counting_doc.insert(ignore_permissions=True)
