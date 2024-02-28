4a8e2c2e067fc3d3d78454f9180b0cb040e3e9ff
Salama <mohamed.salama@lavaloon.com>
2024-02-28 17:59:52 +0200

    fix cron format

--- ksa_compliance/hooks.py
+++ ksa_compliance/hooks.py
@@ -147,7 +147,7 @@ doc_events = {
 
 scheduler_events = {
     "cron": {
-        "57 * * * *": [
+        "0 2 * * *": [
             "ksa_compliance.background_jobs.sync_e_invoices"
         ]
     }
