9bf19c00c90e79ed8e9158ea24d29f02629dc0aa
Muhammad Haggag <muhammad.haggag@lavaloon.com>
2024-03-03 21:57:47 +0200

    Skip additional fields for invoices issued before 2024-03-01

--- ksa_compliance/standard_doctypes/sales_invoice.py
+++ ksa_compliance/standard_doctypes/sales_invoice.py
@@ -1,4 +1,5 @@
 import frappe
+from datetime import date
 
 from ksa_compliance import logger
 from ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_business_settings import ZATCABusinessSettings
@@ -17,6 +18,10 @@ def clear_additional_fields_ignore_list() -> None:
 
 
 def create_sales_invoice_additional_fields_doctype(self, method):
+    if not should_enable_zatca_for_invoice(self.name):
+        logger.info(f"Skipping additional fields for {self.name} because it's before start date")
+        return
+
     settings = ZATCABusinessSettings.for_invoice(self.name)
     if not settings:
         logger.info(f"Skipping additional fields for {self.name} because of missing ZATCA settings")
@@ -54,3 +59,14 @@ def create_sales_invoice_additional_fields_doctype(self, method):
     else:
         si_additional_fields_doc.integration_status = "Ready For Batch"
         si_additional_fields_doc.insert(ignore_permissions=True)
+
+
+def should_enable_zatca_for_invoice(invoice_id: str) -> bool:
+    records = frappe.db.sql("SELECT bv.local_trx_date_time FROM `tabVehicle Booking Item Info` bvii JOIN `tabBooking Vehicle` bv on bvii.parent = bv.name WHERE bvii.sales_invoice = %(invoice)s", {'invoice': invoice_id}, as_dict=True)
+    start_date = date(2024, 3, 1)
+    if records:
+        local_date = records[0]['local_trx_date_time'].date()
+        return local_date >= start_date
+
+    posting_date = frappe.db.get_value('Sales Invoice', invoice_id, 'posting_date')
+    return posting_date >= start_date
