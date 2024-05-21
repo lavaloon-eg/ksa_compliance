83d96b79207600ea6f3b352cd2a7a914e046ff55
Mostafa Essam <mostafaredone@gmail.com>
2024-05-21 13:16:12 +0300

    add tax calculation for sales invoice

--- ksa_compliance/hooks.py
+++ ksa_compliance/hooks.py
@@ -136,9 +136,10 @@ doctype_js = {"Customer": "public/js/customer.js",
 doc_events = {
     "Sales Invoice": {
         "on_submit": "ksa_compliance.standard_doctypes.sales_invoice.create_sales_invoice_additional_fields_doctype",
+        "on_change": "ksa_compliance.standard_doctypes.sales_invoice_item.calculate_tax_amount"
     },
     "Sales Invoice Item": {
-        "on_insert": "ksa_compliance.standard_doctypes.sales_invoice_item.calculate_tax_amount"
+        # "on_insert": "ksa_compliance.standard_doctypes.sales_invoice_item.calculate_tax_amount"
     }
 }
 
--- ksa_compliance/standard_doctypes/sales_invoice_item.py
+++ ksa_compliance/standard_doctypes/sales_invoice_item.py
@@ -1,5 +1,11 @@
 import frappe
+import json
 
 
 def calculate_tax_amount(self, method):
-    print(self.as_dict())
+    if self.items:
+        for item in self.items:
+            item_tax_rate = json.loads(item.item_tax_rate)["Expenses Included In Valuation - KD"]
+            item.custom_tax_total = (item.amount * item_tax_rate) / 100
+            item.custom_total_after_tax = item.amount + item.custom_tax_total
+            item.save()
