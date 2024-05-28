332064075ca72c19315a7eb8419f7e1c7998e055
Mostafa Essam <mostafaredone@gmail.com>
2024-05-28 10:39:56 +0300

    update to set primary address on company change

--- ksa_compliance/ksa_compliance/doctype/zatca_phase_1_business_settings/zatca_phase_1_business_settings.js
+++ ksa_compliance/ksa_compliance/doctype/zatca_phase_1_business_settings/zatca_phase_1_business_settings.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2024, LavaLoon and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("ZATCA Phase 1 Business Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("ZATCA Phase 1 Business Settings", {
+	company(frm) {
+		frappe.call({
+			method: "ksa_compliance.ksa_compliance.doctype.zatca_phase_1_business_settings.zatca_phase_1_business_settings.get_company_primary_address",
+			args:{
+				company: frm.doc.company,
+			},
+			callback: function (r) {
+				console.log(r.message);
+				if (r.message.length === 0) {
+					frm.set_value("address", "")
+				}else{
+					frm.set_value("address", r.message[0].name)
+				}
+			}
+		});
+	},
+});
--- ksa_compliance/ksa_compliance/doctype/zatca_phase_1_business_settings/zatca_phase_1_business_settings.py
+++ ksa_compliance/ksa_compliance/doctype/zatca_phase_1_business_settings/zatca_phase_1_business_settings.py
@@ -3,6 +3,8 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils.data import get_link_to_form
+from frappe.query_builder import DocType
 
 
 class ZATCAPhase1BusinessSettings(Document):
@@ -17,11 +19,28 @@ class ZATCAPhase1BusinessSettings(Document):
 		address: DF.Link
 		company: DF.Link
 		status: DF.Literal["Active", "Disabled"]
-		type_of_transaction: DF.Literal["Simplified", "Standard Tax Invoice", "Both"]
+		type_of_transaction: DF.Literal["Simplified Tax Invoice", "Standard Tax Invoice", "Both"]
 		vat_registration_number: DF.Data | None
 	# end: auto-generated types
 	pass
 
 	def validate(self):
-		if frappe.get_value("ZATCA Business Settings", {"company": self.company}) and self.status == "Active":
-			frappe.throw("ZATCA Phase 2 Business Settings already enabled.", title="Another Setting Already Enabled")
+		business_settings_id = frappe.get_value("ZATCA Business Settings", {"company": self.company})
+		if business_settings_id and self.status == "Active":
+			link = get_link_to_form("ZATCA Business Settings", business_settings_id)
+			frappe.throw(f"ZATCA Phase 2 Business Settings already enabled for company {self.company}: {link}",
+						 title="Another Setting Already Enabled")
+
+
+@frappe.whitelist()
+def get_company_primary_address(company):
+	dynamic_link = DocType("Dynamic Link")
+	address = DocType("Address")
+	query = (
+		frappe.qb.from_(address)
+			.select(address.name)
+			.where(address.is_primary_address == 1)
+			.join(dynamic_link).on(address.name == dynamic_link.parent)
+			.where(dynamic_link.link_name == company)
+	).run(as_dict=1)
+	return query
