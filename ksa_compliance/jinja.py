51b691a68a872e6e48310b23ae6400d329bd64be
Mostafa Essam <mostafaredone@gmail.com>
2024-12-24 18:21:28 +0200

    Update ZATCA Phase 2 print formats

--- ksa_compliance/jinja.py
+++ ksa_compliance/jinja.py
@@ -4,11 +4,14 @@ from base64 import b64encode
 from io import BytesIO
 from typing import cast, Optional
 
-import frappe
 import pyqrcode
-from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
+
+import frappe
 from erpnext.accounts.doctype.pos_invoice.pos_invoice import POSInvoice
+from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
+from erpnext.setup.doctype.branch.branch import Branch
 from frappe.utils.data import get_time, getdate
+from ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_business_settings import ZATCABusinessSettings
 
 
 def get_zatca_phase_1_qr_for_invoice(invoice_name: str) -> str:
@@ -89,3 +92,73 @@ def generate_qrcode(data: str) -> str:
         buffer.seek(0)
         img_str = base64.b64encode(buffer.getvalue()).decode('utf-8')
         return img_str
+
+
+def get_phase_2_print_format_details(sales_invoice: SalesInvoice | POSInvoice) -> dict | None:
+    settings_id = frappe.db.exists(
+        'ZATCA Business Settings', {'company': sales_invoice.company, 'enable_zatca_integration': True}
+    )
+    if not settings_id:
+        return None
+
+    branch_doc = None
+    has_branch_address = False
+    settings = cast(ZATCABusinessSettings, frappe.get_doc('ZATCA Business Settings', settings_id))
+    if settings.enable_branch_configuration:
+        if sales_invoice.branch:
+            branch_doc = cast(Branch, frappe.get_doc('Branch', sales_invoice.branch))
+            if branch_doc.custom_company_address:
+                has_branch_address = True
+    seller_other_id, seller_other_id_name = get_seller_other_id(sales_invoice, settings)
+    buyer_other_id, buyer_other_id_name = get_buyer_other_id(sales_invoice.customer)
+    siaf = frappe.get_last_doc('Sales Invoice Additional Fields', {'sales_invoice': sales_invoice.name})
+    return {
+        'settings': settings,
+        'address': {
+            'street': branch_doc.custom_street if has_branch_address else settings.street,
+            'district': branch_doc.custom_district if has_branch_address else settings.district,
+            'city': branch_doc.custom_city if has_branch_address else settings.city,
+            'postal_code': branch_doc.custom_postal_code if has_branch_address else settings.postal_code,
+        },
+        'seller_other_id': seller_other_id,
+        'seller_other_id_name': seller_other_id_name,
+        'buyer_other_id': buyer_other_id,
+        'buyer_other_id_name': buyer_other_id_name,
+        'siaf': siaf,
+    }
+
+
+def get_seller_other_id(sales_invoice: SalesInvoice | POSInvoice, settings: ZATCABusinessSettings) -> tuple:
+    seller_other_ids = ['CRN', 'MOM', 'MLS', '700', 'SAG', 'OTH']
+    seller_other_id, seller_other_id_name = None, None
+    if settings.enable_branch_configuration:
+        if sales_invoice.branch:
+            seller_other_id = frappe.get_value(
+                'Additional Seller IDs', {'parent': sales_invoice.branch, 'type_code': 'CRN'}, 'value'
+            )
+    if not seller_other_id:
+        for other_id in seller_other_ids:
+            seller_other_id = frappe.get_value(
+                'Additional Seller IDs', {'parent': settings.name, 'type_code': other_id}, 'value'
+            )
+            seller_other_id = seller_other_id.strip() or None if isinstance(seller_other_id, str) else seller_other_id
+            if seller_other_id and seller_other_id != 'CRN':
+                seller_other_id_name = frappe.get_value(
+                    'Additional Seller IDs', {'parent': settings.name, 'type_code': other_id}, 'type_name'
+                )
+                break
+    return seller_other_id, seller_other_id_name or 'Commercial Registration Number'
+
+
+def get_buyer_other_id(customer: str) -> tuple:
+    buyer_other_ids = ['TIN', 'CRN', 'MOM', 'MLS', '700', 'SAG', 'NAT', 'GCC', 'IQA', 'PAS', 'OTH']
+    buyer_other_id, buyer_other_id_name = None, None
+    for other_id in buyer_other_ids:
+        buyer_other_id = frappe.get_value('Additional Buyer IDs', {'parent': customer, 'type_code': other_id}, 'value')
+        buyer_other_id = buyer_other_id.strip() or None if isinstance(buyer_other_id, str) else buyer_other_id
+        if buyer_other_id and buyer_other_id != 'CRN':
+            buyer_other_id_name = frappe.get_value(
+                'Additional Buyer IDs', {'parent': customer, 'type_code': other_id}, 'type_name'
+            )
+            break
+    return buyer_other_id, buyer_other_id_name or 'Commercial Registration Number'
