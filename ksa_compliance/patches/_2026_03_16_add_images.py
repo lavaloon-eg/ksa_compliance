0a116fe9b6b47b072407c2ba62b22a7d6f6f0a95
AMRRAGHEB118 <amrragheb118@gmail.com>
2026-03-16 12:07:25 +0200

    Add ZATCA desktop icon image

---
+++ ksa_compliance/patches/_2026_03_16_add_images.py
@@ -0,0 +1,28 @@
+import os
+
+import frappe
+
+
+def execute():
+    """Add images from the app's public folder to File Documents."""
+    app_path = frappe.get_app_path('ksa_compliance')
+    base_path = os.path.join(app_path, 'public', 'images')
+
+    if not os.path.exists(base_path):
+        frappe.log_error(message=f'Images folder not found: {base_path}', title='ksa_compliance patch')
+        return
+
+    for filename in os.listdir(base_path):
+        if frappe.db.exists('File', {'file_name': filename}):
+            frappe.log(f'Image "{filename}" already exists. Skipping upload.')
+            continue
+
+        full_path = os.path.join(base_path, filename)
+        if os.path.isfile(full_path):
+            with open(full_path, 'rb') as file:
+                file_data = file.read()
+                file_doc = frappe.get_doc(
+                    {'doctype': 'File', 'file_name': filename, 'is_private': 0, 'content': file_data}
+                )
+                file_doc.insert(ignore_permissions=True)
+                frappe.log(f'Added image {filename} to File Documents.')
