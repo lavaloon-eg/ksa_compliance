f9ebf7c62ed526c3f1377e0d3ba707e367e5a65b
AMRRAGHEB118 <amrragheb118@gmail.com>
2026-03-29 15:27:25 +0200

    Remove old images with patch

--- ksa_compliance/patches/_2026_03_16_add_zatca_images.py
+++ ksa_compliance/patches/_2026_03_16_add_zatca_images.py
@@ -4,8 +4,9 @@ import frappe
 
 
 def execute():
-    """Add zatca images from the app's public folder to File Documents."""
-    images = ['zatca-icon.svg']
+    """Add zatca images from the app's public folder to File Documents. Also remove any old versions of the image if they exist."""
+    images_to_add = ['zatca-icon.svg']
+    images_to_delete = ['zatca-icon.png']
     app_path = frappe.get_app_path('ksa_compliance')
     base_path = os.path.join(app_path, 'public', 'images')
 
@@ -13,7 +14,7 @@ def execute():
         frappe.log_error(message=f'Images folder not found: {base_path}', title='ksa_compliance patch')
         return
 
-    for filename in images:
+    for filename in images_to_add:
         if frappe.db.exists('File', {'file_name': filename}):
             frappe.log(f'Image "{filename}" already exists. Skipping upload.')
             continue
@@ -28,3 +29,13 @@ def execute():
                 )
                 file_doc.insert(ignore_permissions=True)
                 frappe.log(f'Added image {filename} to File Documents.')
+
+    for filename in images_to_delete:
+        if frappe.db.exists('File', {'file_name': filename}):
+            files_to_delete = frappe.get_all('File', filters={'file_name': filename})
+            for file in files_to_delete:
+                file_doc = frappe.get_doc('File', file.get('name'))
+                file_doc.delete(ignore_permissions=True)
+                frappe.log(f'Deleted old image {filename} from File Documents. File ID: {file_doc.name}')
+        else:
+            frappe.log(f'Image "{filename}" not found. No deletion needed.')
