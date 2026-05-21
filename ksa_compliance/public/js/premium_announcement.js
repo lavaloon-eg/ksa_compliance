78397ee58e0c9ff291c68930b9340c9c983c53c4
Mostafa Radwan <mostafaredone@gmail.com>
2026-05-21 13:37:13 +0300

    Delete the premium announcement unused code

--- ksa_compliance/public/js/premium_announcement.js
+++
@@ -1,7 +0,0 @@
-frappe.provide("ksa_compliance.premium_announcement");
-
-ksa_compliance.premium_announcement = {
-    open_premium_page() {
-        frappe.set_route("ksa-compliance-premium");
-    }
-};
